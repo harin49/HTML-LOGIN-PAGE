f2ad8c5dbdfa8004d65f02a548b417e6275c96e6
Hari Narayanan <harinarayanan4057@gmail.com>
2016-04-19 02:46:41 +0530

    Update login_1.php

--- login_1.php
+++ login_1.php
@@ -1,29 +1,32 @@
 <?php
 
 session_start();
-require('connect.php');
+require_once('connect.php');
 if(isset($_POST['username']) and isset($_POST['password']))
 {
 	$username = $_POST['username'];
 	$password = $_POST['password'];
 
-$query = "SELECT * FROM 'sample_login' WHERE username='$username' AND password='$password'";
-$result = mysqli_query($query) or die(mysqli_error());
-$count = mysql_num_rows($result);
+$query = "SELECT * FROM sample_login WHERE username='$username' AND password='$password'";
+$result = mysqli_query($connection,$query) or die(mysqli_error($connection));
+$count = mysqli_num_rows($result);
 if($count==1)
 {
-	$_SESSION['username'] = $username;
+	/*$_SESSION['username'] = $username;*/
+	echo "login succesful";
+	echo "<br/>";
+	echo "welcome " .$username."";
 }
 else
 {
 	echo "invalid login details";
 }
 }
-if(isset($_SESSION['username']))
+/*if(isset($_SESSION['username']))
 {
 	$username = $_SESSION['username'];
-	echo "hello".$username ."";
- }
+	echo "hello  ".$username ."";
+ }*/
 
 
 
